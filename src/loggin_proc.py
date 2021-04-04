2d45714f913e96dc7946c1d49e87dc30f82f1a71
Baquetron <baquedano95@gmail.com>
2021-04-05 00:46:33 +0200

    scrapping loop over transfers history list

--- src/loggin_proc.py
+++ src/loggin_proc.py
@@ -3,12 +3,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+import time
+
+_FUTMONDO_ADDRESS = "https://www.futmondo.com/"
 
 def execute():
     loggin_page()
 
 def loggin_page():
-    print("loggin_page")
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-maximized")
+    options.add_argument('disable-infobars')
+    try:
+        driver = webdriver.Chrome("../tools/bin/gc84/chromedriver.exe", options=options)
+    except:
+        driver = webdriver.Chrome("../tools/bin/chromedriver", options=options)
+
+    driver.get("file:///Users/inigo/Sandbox/fut_est/data/transfers_history.html")
+    # check transfer history list is visible
+    elem_transfer_history = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="pressReleases"]')))
+    # get all elems from list
+    l_transfer_history = driver.find_elements_by_css_selector("#pressReleases > ul > li > ul")
+ 
+    for i, row in enumerate(l_transfer_history):
+        line = str(row.text)
+        print(line)
 
 if __name__ == "__main__":
     execute()
