16327026aab8b60ac5ae297e29538ae188832266
Baquetron <baquedano95@gmail.com>
2021-04-04 20:38:42 +0200

    Initial dirs

---
+++ src/loggin_proc.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+def execute():
+    loggin_page()
+
+def loggin_page():
+    print("loggin_page")
+
+if __name__ == "__main__":
+    execute()
---
+++ src/main.py
@@ -0,0 +1,4 @@
+import loggin_proc
+
+if __name__ == "__main__":
+    loggin_proc.execute()
